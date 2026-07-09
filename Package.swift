96f61dfc9ec87df4c0b681d1fa1e66e5a494b62b
m2构造机 <zhanggaoqi@aobi.com>
2026-07-09 19:07:53 +0800

    [Branch Release] v2.0.3-dev-1482588 - develop
    - 更新所有 XCFrameworks 到 2.0.3-dev-1482588
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "BTSimpleHTTPNetworking",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/BTSimpleHTTPNetworking/2.0.3-dev-1481308/BTSimpleHTTPNetworking.xcframework.zip",
-            checksum: "9e403e4d5c2a6255aaf3bd7594fabd0b2852f407f57dae0711158084222e3388"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/BTSimpleHTTPNetworking/2.0.3-dev-1482588/BTSimpleHTTPNetworking.xcframework.zip",
+            checksum: "691ff3f521f2997ba1ef6fde0fedfc178f3505681b2ce73e5d329f0dbc18c3fe"
         )
     ]
 )
